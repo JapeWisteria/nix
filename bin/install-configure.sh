e7fb500ec825e100203b5e03d8bdd91a6dab8779
Jeff Wyonch <jeff.wyonch@gmail.com>
2018-03-09 11:02:48 -0500

    store some terminal apps and configs for mac

---
+++ bin/install-configure.sh
@@ -0,0 +1,11 @@
+
+# Install brew on mac
+# https://brew.sh/
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install some apps
+brew install wget
+brew install dos2unix
+brew install gcal #fixes issues with mac cal
+
+
